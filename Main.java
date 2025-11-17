b2549350af908cb41ac36b0a573edf23aaf05a4d
YazatS123 <yazat.sinha@gmail.com>
2025-11-17 01:05:14 -0600

    Updated PositionFinder.java and Main.java
    Finished PositionFinder.java, updated Main.java to align and made names.txt more scalable, and added a solution.txt for the POC to verify solutions once a comprehensible solution is generated

--- Main.java
+++ Main.java
@@ -5,24 +5,35 @@ public class Main {
     public static void main(String[] args) throws Exception {
         
         // Get values from names.txt and save them to the HashMap
-        HashMap<String, Integer[]> df = new HashMap<>();
+        HashMap<String, String[]> df = new HashMap<>();
         String[] names = new String[0];
         Scanner scanner = new Scanner(new File("names.txt"));
+        int categoryNum = scanner.nextInt();
+        String[] categories = new String[0];
+        HashSet<String> catCheck = new HashSet<>();
         while (scanner.hasNext()) {
             // Read data on a line
             String name = scanner.next();
             names = Arrays.copyOf(names, names.length + 1);
             names[names.length - 1] = name;
-            Integer[] data = new Integer[3];
-            data[0] = scanner.nextInt();
-            data[1] = scanner.nextInt();
-            data[2] = scanner.nextInt();
+            String[] data = new String[categoryNum];
+            for (int i = 0; i < categoryNum; i++) {
+                data[i] = scanner.next();
+                // Fill the categories list
+                if (!catCheck.contains(data[i])) {
+                    categories = Arrays.copyOf(categories, categories.length + 1);
+                    categories[categories.length - 1] = data[i];
+                    catCheck.add(data[i]);
+                }
+            }
             df.put(name, data);
         }
         
+        
         // Test output
-        for (String name : names) {
+        /* for (String name : names) {
             System.out.println(name + ": " + df.get(name)[0] + " " + df.get(name)[1] + " " + df.get(name)[2]);
-        }
+        } */
+        HashMap<String, Double[]> positions = PositionFinder.plot(df, names, categories);
     }
 }
--- PositionFinder.java
+++ PositionFinder.java
@@ -1,7 +1,43 @@
 import java.util.*;
 
 public class PositionFinder {
-    public HashMap<String, Integer[]> plot(HashMap<String, Integer[]> in, String[] names) {
-        return in;
+    public static HashMap<String, Double[]> setCoords(String[] categories) {
+
+        // This class will have to be revamped entirely for future versions, because it can only assign coordinates to 3 categories, but for a proof of concept this will do just fine.
+        HashMap<String, Double[]> out = new HashMap<>();
+
+        Double[] data = {3.0, 0.0};
+        out.put(categories[0], data);
+        data[0] = -1.5;
+        data[1] = 1.5 * Math.sqrt(3);
+        out.put(categories[1], data);
+        data[1] *= -1;
+        out.put(categories[2], data);
+
+        return out;
+    }
+    public static HashMap<String, Double[]> plot(HashMap<String, String[]> in, String[] names, String[] categories) {
+        HashMap<String, Double[]> df = new HashMap<>(); // Will store names and points at the end
+        HashMap<String, Double[]> points = setCoords(categories); // Holds the "points" for the categories
+        double[] percentages = new double[categories.length];
+        for (int i = 0; i < categories.length; i++) percentages[i] = 1.0 * Math.pow(0.5, i);
+        // Percentages should be 0.5, 0.25, 0.125 for POC
+        for (String name : names) {
+
+            String[] currentData = in.get(name); // pull the current data
+            double x = 0;
+            double y = 0;
+            for (int i = 0; i < currentData.length; i++) {
+                // Needs to be modified for scalability, but for now should work for three categories
+                x += percentages[i] * points.get(currentData[i])[0];
+                y += percentages[i] * points.get(currentData[i])[1];
+            }
+            Double[] pos = {x, y}; // Should hold position of student
+            df.put(name, pos);
+            
+        }
+        
+        
+        return df;
     }
 }
